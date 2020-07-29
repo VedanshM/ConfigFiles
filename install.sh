1355cbbaafef6f2dd63ff46eed7332661cbccd3d
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-07-29 18:12:12 +0530

    no link,copy from installFile; basically functional now.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ homeFiles=$(find $DIR -maxdepth 1 -type f)
 
 for file in ${homeFiles[@]}; do
 	bname=$(basename $file)
-	echo $file  $bname
-	#cp -li $file $HOME/$bname
+	echo $bname
+	cp -i $file $HOME/$bname
 done
 
